972c58104dc7a0388347dd00438bff63f8237169
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-01-04 23:45:58 -0500

    Add ioapi_buf.c to Android and CMake builds

--- Android.mk
+++ Android.mk
@@ -2,5 +2,5 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libminizip
-LOCAL_SRC_FILES := ioapi.c ioandroid.c unzip.c zip.c
+LOCAL_SRC_FILES := ioapi.c ioapi_buf.c ioandroid.c unzip.c zip.c
 include $(BUILD_STATIC_LIBRARY)
