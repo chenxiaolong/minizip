e5ccc7f4dbbc0c6efb01c445181726d6e5d21d96
Andrew Gunnerson <chenxiaolong@cxl.epac.to>
2017-03-26 14:18:29 -0400

    Remove unneeded Android.mk
    Signed-off-by: Andrew Gunnerson <chenxiaolong@cxl.epac.to>

--- Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libminizip
-LOCAL_SRC_FILES := ioapi.c ioapi_buf.c ioandroid.c unzip.c zip.c
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-include $(BUILD_STATIC_LIBRARY)
